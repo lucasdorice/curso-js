007202f65ec07bff9cd7389d996608d5151c5186
lucasdorice <lucasdoricesantos@gmail.com>
2025-12-18 18:19:16 -0300

    first commit

---
+++ aula001/index.js
@@ -0,0 +1,8 @@
+console.log('Lucas Dorice'); // String = Texto
+console.log("Lucas Dorice");
+console.log(`Lucas Dorice`)
+console.log(`'Lucas' "Dorice"`);
+
+console.log(123456, 15.85); // Number = Número
+
+console.log(123456, 15.85, 'Lucas Dorice');
